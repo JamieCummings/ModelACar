e5cdbe39625d2d011f7dd1c9454aee3d0c8217ba
Jamie Cummings <jamiecummings@macbook-pro-2.local>
2018-08-20 14:32:42 -0500

    Initial Commit

---
+++ ModelACar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ModelACar
+//
+//  Created by Jamie Cummings on 8/20/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
